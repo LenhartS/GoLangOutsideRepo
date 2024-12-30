1b883e89a6e81d40c9d964c877f2ed3d6e8eb3a0
LenhartS <lenhart.steve@gmail.com>
2024-12-30 21:29:31 +0000

    Update mainSecondPractice.go

--- mainSecondPractice.go
+++ mainSecondPractice.go
@@ -3,7 +3,8 @@ package mainSecondPractice
 //import "fmt"
 
 func SayHelloOutside() string {
-	Return "You went Outside to a the GoLangOutsideRepo Repo on git hub for this function"
+	S1 := "You went Outside to a the GoLangOutsideRepo Repo on git hub for this function"
+	return s1
 }
 //func SayHelloOutside() {
 //	fmt.Printf("\nHellofromOutside")
